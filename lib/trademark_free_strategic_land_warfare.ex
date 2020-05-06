1662bd320f25a66e4f5dbb73177711df5dbfa809
Alan Voss <alanvoss@hotmail.com>
2020-05-06 11:09:46 -0500

    initial commit

---
+++ lib/trademark_free_strategic_land_warfare.ex
@@ -0,0 +1,18 @@
+defmodule TrademarkFreeStrategicLandWarfare do
+  @moduledoc """
+  Documentation for `TrademarkFreeStrategicLandWarfare`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TrademarkFreeStrategicLandWarfare.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/trademark_free_strategic_land_warfare_test.exs
@@ -0,0 +1,8 @@
+defmodule TrademarkFreeStrategicLandWarfareTest do
+  use ExUnit.Case
+  doctest TrademarkFreeStrategicLandWarfare
+
+  test "greets the world" do
+    assert TrademarkFreeStrategicLandWarfare.hello() == :world
+  end
+end
