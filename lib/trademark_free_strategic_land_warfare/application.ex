3a9992029ca0f1cb397558ff57b7c004c01a0b38
Alan Voss <alanvoss@hotmail.com>
2020-05-31 16:38:45 -0500

    adding application with Task Supervisor

---
+++ lib/trademark_free_strategic_land_warfare/application.ex
@@ -0,0 +1,11 @@
+defmodule TrademarkFreeStrategicLandWarfare.Application do
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {Task.Supervisor, name: TrademarkFreeStrategicLandWarfare.TaskSupervisor}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+end
